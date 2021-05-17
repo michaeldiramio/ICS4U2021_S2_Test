ef472f1da0ac0e1287486c11d150dffe85eff55a
rupcicj <>
2021-05-17 16:30:48 +0000

    Added Pig.java

--- Main.java
+++ Main.java
@@ -9,6 +9,7 @@ public class Main {
 
     // add animals here
     animals.add(new Dog());
+    animals.add(new Pig());
 
     System.out.println("BLAH");
 
---
+++ Pig.java
@@ -0,0 +1,7 @@
+public class Pig extends Animal {
+
+  public void makeNoise() {
+    System.out.println("Oink");
+  }
+
+}
