e7ac6732fd1c8d388f3c0bd8d204a8992330030a
Michael DiRamio <>
2021-05-17 15:23:52 +0000

    Added animal framework

--- Main.java
+++ Main.java
@@ -1,8 +1,25 @@
+import java.util.ArrayList;
+
 public class Main {
   
   public static void main(String[] args) {
     
-    System.out.println("Kaiden Gwynne");
+    ArrayList<Animal> animals = new ArrayList<>();
+
+    // add animals here
+    animals.add(new Dog());
+
+
+
+    // print em all
+   /* for(int i = 0; i < animals.size(); i++) {
+      animals.get(i).makeNoise();
+    }*/
+
+    for(Animal a : animals) {
+      a.makeNoise();
+    }
+
     
   }
 
